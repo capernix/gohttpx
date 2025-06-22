d48278c04f14c5148a221d632712e75cf0d47a81
capernix <marvinsukumar1@gmail.com>
2025-06-22 11:08:25 +0530

    Fixed the bug where the colons were missing

--- models/note.go
+++ models/note.go
@@ -7,8 +7,8 @@ import (
 
 type Note struct {
 	ID      int    `json:"id"`
-	Title   string `json"title"`
-	Content string `json"content"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 }
 
 var (
